4138c663bc707ed378bebb85ce246517df73074e
Pongstr <twiz.tickler@gmail.com>
2013-09-02 14:31:35 +0800

    updates
    * set all assets to local resource
    * switched from font-awesome to bs glyphicons

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -8,120 +8,91 @@
  * Requires: jQuery v1.10.1 and Bootstrap 3.0
  * ========================================================================= */
 
-$(window).load(function(){ "use strict";
+$(window).load(function () { "use strict";
   
   // Make example code pretty
-  if ( $('.prettyprint').length > 0 ) {
-    window.prettyPrint && prettyPrint();
+  if ($('.prettyprint').length > 0) {
+    window.prettyPrint && prettyPrint()
   }
 
-  /* Sticky Menu
-   * ============================================ */
-    var headerHeight = $('header').outerHeight();
+    // Sticky Menu
+    // ==========================================
+    var headerHeight = $('header').outerHeight()
    
-    $(window).scroll(function(){
-      //if scrolled down more than the header's height
-      if ($(window).scrollTop() > headerHeight ){
-          $('.substitute-btn').addClass('stick');          
+    $(window).scroll(function (){ 
+      if ($(window).scrollTop() > headerHeight){
+          $('.substitute-btn').addClass('stick')        
         } else {
-          $('.substitute-btn').removeClass('stick');
+          $('.substitute-btn').removeClass('stick')
       }
-    });   
+    })
 
 
-  /* Smooth scroll for internal links
-   * ============================================ */
+  // Smooth scroll for internal links
+  // ============================================
   $('#sidebar a[href^="#"]').on('click', function (e) {
+    e.preventDefault()
     
-    e.preventDefault();
-    
-    var target = this.hash,
-       $target = $(target);
-       
+    var  target = this.hash
+    var $target = $(target)
+               
     $('html, body').stop().animate({
-      
-        'scrollTop': $target.offset().top  - 10
-        
-    }, { duration: 1250, easing: 'easeInOutExpo'}, function () {
-      
-        window.location.hash = target;
-        
-        return false;
-    });
+      'scrollTop': $target.offset().top  - 65
+    }, function () {
+      window.location.hash = target
+      return false
+    })
 
 
-    /* Sub-Pages Navigation
-     * ============================================ */
-    if ( $('#sidebar a').length > 0 ) {
-      
-      $('#sidebar a').removeClass('active');
-      
-      $(this).addClass('active');
-      
+    // Sub-Pages Navigation
+    // ==========================================
+    if ($('#sidebar a').length > 0) {
+      $('#sidebar a').removeClass('active')
+      $(this).addClass('active')
     }
     
-  });
-  
-  /* Enable Tooltip for Substitue Nav
-   * ============================================ */
-  $('.substitute-btn a').each(function(){
-    
-    $(this).hover(function(){
-      
+  })
+
+
+  // Enable Tooltip for Substitue Nav
+  // ============================================
+  $('.substitute-btn a').each(function () {
+    $(this).hover(function () {
       $(this).tooltip('show');
-      
-    });
-    
-  });
+    })
+  })
   
   
-  /* Load Instagram Stuff
-   * ============================================
-   * To use this function, the variable target should be the same
-   * as your selector i.e., <div id="<target>"> and the variable
-   * showValue is the number of media you would like to show i.e., 8
-   *
-   */
-  // function loadGram( target, showValue ){
-  //   var usr = '39666111',
-  //       tkn = usr + '.1fb234f.c3901000b4944a549fd5fd2310c63780',
-  //       tgt = '#' + target;
+  // Load Instagram Stuff
+  // ============================================
+  function loadGram (target, show) {
+    var usr = '39666111'
+    var tkn = '39666111.1fb234f.c3901000b4944a549fd5fd2310c63780'
+    var tgt = '#' + target
     
-  //   if ( $(tgt).length > 0 ){
-  //     $(tgt).pongstgrm({
-  //       accessId    : usr,
-  //       accessToken : tkn,
-  //       show        : target,
-  //       count       : showValue,
-  //       pager       : true
-  //     });
-  //   }
-    
-  //   $('[data-paginate="' + target + '"]')
-  //     .removeClass('btn-success')
-  //     .addClass('btn-danger text-center');
-  // }
+    if ($(tgt).length > 0){
+      $(tgt).pongstgrm({
+          accessId    : usr
+        , accessToken : tkn
+        , show        : target
+        , count       : show
+        , pager       : true
+      })
+    }
+  }
   
-  // loadGram( 'recent', 4 );
-  // loadGram( 'liked',  8 );
-  // loadGram( 'feed',   8 );
+  loadGram('recent', 4);
+  loadGram('liked' , 4);
+  loadGram('feed'  , 4);
 
-    var usr = '39666111',
-        tkn = usr + '.1fb234f.c3901000b4944a549fd5fd2310c63780';
-
-    $('.recent').pongstgrm({
-      accessId: usr,
-      accessToken: tkn
-    });
 
 }); /*! end window.load */
 
   
 // Hide iOS/Android address bar after page loads
 // =============================================
-
-if (navigator.userAgent.match( /(iPod|iPhone|iPad)/ )) { 
-  window.addEventListener( 'load', function() {
-    setTimeout( function() { window.scrollTo(0, 0); }, 0);
-  });
+if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) { 
+  window.addEventListener('load', function () {
+    setTimeout( function() { window.scrollTo(0, 0) }, 0)
+  })
 }
--- docs/assets/js/pongstagr.am.js
+++ docs/assets/js/pongstagr.am.js
@@ -8,73 +8,87 @@
  * Requires: jQuery v1.10.1 and Bootstrap 3.0
  * ========================================================================= */
 
-;(function ($, window, document, undefined){ "use strict";
++function ($) { "use strict";
   
-  var html = {
-    
-    gallery: function (element, pager) {
-      var tgt = $(element);
-      var btn = '<div class="row text-center"><button class="btn btn-success btn-lg col-xs-12 col-md-6 col-lg-2 col-lg-offset-5">more</button>';
-      
-      $(element).addClass('pongstagram row');
 
-      if (pager === null || pager === true) {
-        $(element).after(btn);
+  // PRIVATE METHODS
+  // =============================
+
+
+  var Pongstgrm = {
+
+    defaults: {
+      
+        // USER AUTHENTICATION
+          accessId:     null    // user id
+        , accessToken:  null    // acccess token
+        
+        // DISPLAY OPTIONS
+        , show:         null    // options: 'profile', 'recent', 'feed', 'liked', 'user'
+        , count:        null    // options: 1(min) - 40(max), instagram limits the maximum number of media to 40
+        , likes:        null    // options: true or false (enable/disable like count)
+        , comments:     null    // options: true or false (enable/disable comment count)
+      
+      }    
+    
+    , container: function (element,show) {
+        var btn = $('<button />').attr({
+            'data-paginate': show
+          , 'class': 'btn btn-block btn-success'
+        }).text('Load more')
+        
+        $(element)
+          .attr('data-type', show)
+          .addClass('pongstgrm row')
+          .append(btn)
+        
+        return
       }
-      
-      return true;
-    }
-    
-    
-    , modal: function (id,src,cpt) {
-      var mdlhdr = '<div class="modal-header"></div>';
-      var mdlbdy = '<div class="modal-body"><div id="'+id+'-lg-ldr" class="spinner"></div><img src="'+src+'" alt="'+cpt+'" id="'+id+'-lg"></div>';
-      var mdlftr = '<div class="modal-footer"><button type="button" class="btn btn-small btn-primary" data-dismiss="modal">Close</button></div>';
-      
-      var modal  = '<div id="'+ id +'" class="modal fade" tabindex="-1" role="dialog">';
-          modal += '<div class="modal-dialog">';
-          modal += '<div class="modal-content">';
-          modal += mdlbdy + mdlftr;
-      
-      $('body').append(modal);
-      
-      html.preload('#'+id+'-lg');
-      
-      $('#'+id).on('hidden.bs.modal', function () {
-        $(this).remove();
-      });
-      
-      return true;
-    }
-    
-    
-    , preload: function (imgid) {
-      var $img = $(imgid);
-      var  ttl = $img.length;
-      var  spn = imgid+'-ldr';
-      var  pre = 0;
-      
-      $img.hide();
-      
-      $img.load( function () {
-        if ( ++pre === ttl ) {
+
+
+      /* Authentication */
+    , access: function (id1,id2) {
+        var access = (id1 !== null || id2 !== null) ? true : false
+        return access
+      }
+
+
+      /* Request */
+    , request: function (request,count,id,token,target) {
+        var apiurl = 'https://api.instagram.com/v1/users/'
+        var rcount = (count !== null) ? 
+          '?count=' +  count + '&access_token=' + token :
+          '?count=' +    4   + '&access_token=' + token
+        
+        switch (request) {
+          case "liked":
+          var liked = apiurl + 'self/media/liked' + rcount
+          break
           
-          $img.fadeIn('normal');
+          case "feed":
+          var feed = apiurl + 'self/feed' + rcount
+          break
           
-          $(spn)
-            .fadeOut('fast')
-            .remove();
+          case "profile":
+          var profile = apiurl + id + '?access_token=' + token
+          break
+          
+          case "recent":
+          case null:
+          var recent = apiurl + id + '/media/recent' + rcount
+          this.ajx(recent,target)
+          break
         }
-      });
-      
-      return true;
-    }
-  }; 
-  
-  
-  var instagram = {
-    
-      ajx: function (apiurl,target) {
+        
+        this.container(target,request)
+        return
+      }
+
+
+      /* Ajax */
+    , ajx: function (apiurl,target) {
+        var self = this
+        
         $.ajax({
             method   : "GET"   
           , url      : apiurl
@@ -83,151 +97,97 @@
           , success  : function(data){
             
             $.each(data.data, function (a,b) {
-              // MEDIA VARIABLES
-              var mVideo     = (b.videos !== undefined ) ? b.videos.standard_resolution.url : ''
-                , mLikes     = (b.likes.count !== null) ? b.likes.count : '0'
-                , mComments  = (b.comments.count !== null) ? b.comments.count : '0'
-                , mCaption   = (b.caption !== null) ? (b.caption.text !== null) ? 
-                                   b.caption.text : '' : b.user.username;
-              // THUMBNAIL BLOCK
-              var thmbnail  = '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">';
-                  thmbnail += '<div class="thumbnail">';
-                  thmbnail += '<div id="'+ b.id +'-thmb-ldr" class="spinner"></div>';
-                  thmbnail += (b.type === 'video') ? '<i class="icon-play"></i>' : '';
-                  thmbnail += '<img src="'+ b.images.low_resolution.url +'" alt="" id="'+ b.id +'-thmb" data-target="'+ b.id +'">';
-                  thmbnail += '</div>';
-                  thmbnail += '</div>';
               
-              $(target).append(thmbnail);
-              
-              html.preload('#'+ b.id +'-thmb');
-              
-              $('[data-target="'+ b.id +'"]').on('click', function (e) {
-                
-                html.modal(b.id,b.images.standard_resolution.url,mCaption);
-                
-                $('#'+b.id).modal('show');
-                
-              });
-            });
-            instagram.pagr(data.pagination.next_url,target);
+            })
+            
           }
-        });
-
-        return true;
+        })
+        
+        return
       }
 
 
-    , pagr: function (next,target) {
+      /* Load more */
+    , more: function (next,target) {
+        var btn = 'test'
+        
         if (next === undefined || next === null) {
-          
-          $('.btn').on('click', function (e) {
-            
-            e.preventDefault();
+          $(btn).on('click', function (e) {
+            e.preventDefault()
             
             $(this)
               .attr('disabled','disabled')
-              .unbind(e);
-            
-          });
+              .unbind(e)
+          })
           
-          return false;
-          
+          return false
         } else {
-          
-          $('.btn').on('click', function (e) {
+          $(btn).on('click', function (e) {
+            e.preventDefault()
             
-            e.preventDefault();
-            
-            instagram.ajx(next,target);
-            
-            $(this).unbind(e);
+            $(this).unbind(e)
+          })
           
-          });
-          
-          return true;
+          return true
         }
-    }
-
-
-    , rqst: function (request,count,id,token,target,pager) {
-        var apiurl  = 'https://api.instagram.com/v1/users/';
-        var more    = (request === null) ? 'recent' : request;
-        var rcount  = (count !== null) ? 
-          '?count=' +  count + '&access_token=' + token :
-          '?count=' +    4   + '&access_token=' + token ;
-
-        switch (request) {
-          case "liked":
-            var liked = apiurl + 'self/media/liked' + rcount;
-            instagram.ajx(liked,target);
-            break;
-            
-          case "feed":
-            var feed = apiurl + 'self/feed' + rcount;
-            instagram.ajax(feed,target);
-            break;
-            
-          case "profile":
-            break;
-            
-          case null:
-          case "recent":
-            var recent = apiurl + id + '/media/recent' + rcount;
-            instagram.ajx(recent,target);
-            break;
-          
-        }
-        html.gallery(target,pager);
+        
       }
-  };
-  
-  
-  function access (id, token) {
-    if ( id !== null || token !== null ) {
-      return true;
-    } else {
-      // console.log('Please check whether your Access ID and Access Token if it\'s valid.' );
-      // console.log('You may visit http://instagram.com/developer/authentication/ for more info.');      
-      return false;
-    }
+
+
+      /* Preload image */
+    , preload: function (imgid) {
+        var $img = $(imgid)
+        var  ttl = $img.length
+        var  spn = imgid+'-ldr'
+        var  pre = 0
+        
+        $img.hide().load(function () {
+          if (++pre === ttl) {
+            $img.fadeIn()
+            $spn.fadeOut().remove()
+          }
+        })
+        
+        return      
+      }
   }
-  
-  
+
+
 
   // PONGSTAGR.AM PLUGIN DEFINITON
   // =============================
+  
   $.fn.pongstgrm = function (option) {
-    var $this   = $(this)
-      , options = $.extend({}, $.fn.pongstgrm.defaults, option);
-    
-    
-    return this.each( function (i, element) {
+    var opt = $.extend({}, Pongstgrm.defaults, option)
 
-      if ( access(options.accessId, options.accessToken) !== false ) {
-        instagram.rqst(options.show,options.count,options.accessId,options.accessToken,element,options.pager);
+    return this.each(function () {
+      var subj = $(this)[0]
+
+      if(Pongstgrm.access(opt.accessId,opt.accessToken) !== null) {
+        Pongstgrm.request(opt.show,opt.count,opt.accessId,opt.accessToken,subj)
       }
-    });
-  };  
-  
-  
-  
+    })
+  }
+
+
+
+
   // PONGSTAGR.AM DEFAULT OPTIONS
   // ============================  
-  $.fn.pongstgrm.defaults = {
   
-    // USER AUTHENTICATION
-      accessId:     null    // user id
-    , accessToken:  null    // acccess token
+  // $.fn.pongstgrm.defaults = {
+  
+  //   // USER AUTHENTICATION
+  //     accessId:     null    // user id
+  //   , accessToken:  null    // acccess token
     
-    // DISPLAY OPTIONS
-    , show:         null    // options: 'recent', 'feed', 'liked', 'user'
-    , count:        null    // options: 1(min) - 40(max), instagram limits the maximum number of media to 40
-    , pager:        null    // options: true or false (enables/disable load more button)
-    , likes:        null    // options: true or false (enable/disable like count)
-    , comments:     null    // options: true or false (enable/disable comment count)
+  //   // DISPLAY OPTIONS
+  //   , show:         null    // options: 'profile', 'recent', 'feed', 'liked', 'user'
+  //   , count:        null    // options: 1(min) - 40(max), instagram limits the maximum number of media to 40
+  //   , likes:        null    // options: true or false (enable/disable like count)
+  //   , comments:     null    // options: true or false (enable/disable comment count)
   
-  };
+  // }
 
 
-})(jQuery, window, document);
+}(window.jQuery);
